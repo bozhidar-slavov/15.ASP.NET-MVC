8bb0157a9dbd19aceeca80279c8cd4a86118a26c
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2020-01-16 16:52:24 +0200

    Edit

--- ForumSystem/Source/Data/ForumSystem.Data.Models/VoteType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ForumSystem.Data.Models
-{
-    public enum VoteType
-    {
-        Negative = -1,
-        Neutral = 0,
-        Positive = 1
-    }
-}
--- ForumSystem/Source/Web/ForumSystem.Web.Infrastructure/Mapping/IMapFrom.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ForumSystem.Web.Infrastructure.Mapping
-{
-    public interface IMapFrom<T>
-    {
-    }
-}
